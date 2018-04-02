fab21d8ff9600c52c560b360df84c5bfeac95235
viczhou <wi19@qq.com>
2018-04-02 21:16:21 +0800

    已处理

--- pages/main/index/index.js
+++ pages/main/index/index.js
@@ -1,3 +1,5 @@
+var sliderWidth = 96;
+
 Page({
     data: {
         active: 1,
@@ -6,7 +8,30 @@ Page({
         grids: [0, 1, 2, 3, 4, 5],
         imgs: ['print', 'shop_info', 'qr_code', 'menu_manger', 'qr_code_manger', 'shop_status'],
         img_title: ['打印机', '餐厅信息', '查看桌码', '菜品管理', '桌码管理', '店铺状态'],
-        urls: ['print/print', 'restinfo/restinfo', 'qrcode/qrcode', 'menumanger/menumanger', 'qrcodemanger/qrcodemanger', 'reststatus/reststatus']
+        urls: ['print/print', 'restinfo/restinfo', 'qrcode/qrcode', 'menumanger/menumanger', 'qrcodemanger/qrcodemanger', 'reststatus/reststatus'],
+        tabs: ["全部", "退单", "异常"],
+        activeIndex: 0,
+        sliderOffset: 0,
+        sliderLeft: 0,
+        data: [{ 'table': 9, 'price': '425.00', 'time': '2018-03-15 15:30:20', 'pid': '2018030500001' }, { 'table': 49, 'price': '475.00', 'time': '2018-03-15 15:30:20', 'pid': '2018030500001' }, { 'table': 45, 'price': '275.00', 'time': '2018-03-15 15:30:20', 'pid': '2018030500001' }, { 'table': 5, 'price': '755.00', 'time': '2018-03-15 15:30:20', 'pid': '2018030500001' }, { 'table': 46, 'price': '25.00', 'time': '2018-03-15 15:30:20', 'pid': '2018030500001' }]
+
+    },
+    onLoad:function(){
+        var that = this;
+        wx.getSystemInfo({
+            success: function (res) {
+                that.setData({
+                    sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+                });
+            }
+        });
+    },
+    topbarClick: function (e) {
+        this.setData({
+            sliderOffset: e.currentTarget.offsetLeft,
+            activeIndex: e.currentTarget.id
+        });
     },
     tabBarClick: function (e) {
         var bar = wx.getSystemInfoSync().windowWidth / 4
